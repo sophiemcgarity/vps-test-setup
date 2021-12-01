3b625f66c939648022372d1e22f7b4096c645466
Sophie McGarity <sophiemcgarity7@gmail.com>
2021-11-30 16:50:41 -0800

    local storage

--- src/index.js
+++ src/index.js
@@ -2,65 +2,27 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 
-let users = {
-  1: {
-    id: '1',
-    username: 'Robin Wieruch',
-  },
-  2: {
-    id: '2',
-    username: 'Dave Davids',
-  },
-};
-
-let messages = {
-  1: {
-    id: '1',
-    text: 'Hello World',
-    userId: '1',
-  },
-  2: {
-    id: '2',
-    text: 'By World',
-    userId: '2',
-  },
-};
+import models, { sequelize } from './models';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 
-app.get('/users', (req, res) => {
-  return res.send(Object.values(users));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
 });
 
-app.get('/users/:userId', (req, res) => {
-  return res.send(users[req.params.userId]);
-});
-
-app.get('/messages', (req, res) => {
-  return res.send(Object.values(messages));
-});
-
-app.get('/messages/:messageId', (req, res) => {
-  return res.send(messages[req.params.messageId]);
-});
-
-app.post('/users', (req, res) => {
-  return res.send('POST HTTP method on user resource');
-});
-
-app.put('/users/:userId', (req, res) => {
-  return res.send(
-    `PUT HTTP method on user/${req.params.userId} resource`,
-  );
-});
-
-app.delete('/users/:userId', (req, res) => {
-  return res.send(
-    `DELETE HTTP method on user/${req.params.userId} resource`,
-  );
-});
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
